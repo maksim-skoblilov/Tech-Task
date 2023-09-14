4fd1d9b84388ef43babb6811084422590b37fc9b
Maksim Skoblilov <maksim.skoblilov@akvelon.com>
2023-09-14 12:19:58 +0400

    Added Dockerode module

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,6 @@
+var Docker = require('dockerode');
+var docker = new Docker({socketPath: '/var/run/docker.sock'});
+console.log(docker);
+
 let message: string = "Hello, World!";
 console.log(message);
